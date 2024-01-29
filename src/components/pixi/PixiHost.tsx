6578a5d1e21bed15d7ddf5d9537d135f501e9f0f
danbermantech <danbermantech@gmail.com>
2024-01-29 01:32:32 -0500

    Build Fix

--- src/components/pixi/PixiHost.tsx
+++ src/components/pixi/PixiHost.tsx
@@ -1,10 +1,7 @@
 import { Sprite, } from '@pixi/react';
 import bg from '$assets/bg.png';
-import Race, { WrappedStage } from './Race';
-import PlayerPieces from './PlayerPieces';
-import BoardSpaces from './BoardSpaces';
+import { WrappedStage } from './Race';
 import RoundCounter from './RoundCounter';
-import Paths from './Paths';
 import Board from './Board';
 
 const boardWidth = (()=>window.innerWidth - 512)();
@@ -13,14 +10,12 @@ const boardHeight = (()=>window.innerHeight - 32)();
 
 export const PixiHost = () =>
 {
-  // const players = useSelector((state:StoreData) => state.players);
   return (
     //@ts-expect-error this is a WrappedStage bug
     <WrappedStage className="w-full mx-auto rounded-xl" width={boardWidth} height={boardHeight} options={{ backgroundColor: 0xffffff, antialias: true }}>
       <Sprite x={0} y={0} width={boardWidth} height={boardHeight} image={bg} scale={{x:boardWidth/1920, y: boardHeight/1080}} />
       <RoundCounter />
       <Board />
-      {/* <Race /> */}
     </WrappedStage>
   );
 };
