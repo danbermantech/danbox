6bc9991b6164f177437b5381d0ae6a436eb11a85
danbermantech <danbermantech@gmail.com>
2024-02-05 00:48:56 -0500

    SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import "./assets/index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
   <Provider store={store}>
     <App />
+    <SpeedInsights />
   </Provider>,
   // </React.StrictMode>
 );
