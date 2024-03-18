2d381bd8ca0e8b486e9ff480441a5b4b4b5b0cf0
danbermantech <danbermantech@gmail.com>
2024-03-17 21:15:26 -0400

    scoreboard style update trivia results style update player piece text style update removing some board paths

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,7 +1,6 @@
 import { GAME_MODE, StoreData } from "$store/types";
 import { useSelector } from "react-redux"
 import PlayerCard from "./PlayerCard";
-import { CopyAllRounded } from "@mui/icons-material";
 import { usePeer } from "$hooks/usePeer";
 import QRShare from "./QRShare";
 import {bg as bgImage} from '$assets/images.ts'
@@ -13,19 +12,25 @@ const ScoreBoard = () => {
   const activePlayers = useSelector((state:StoreData)=>state.game.activePlayers);
   const gameMode = useSelector((state:StoreData)=>state.game.mode);
     return <div className="text-black flex-grow p-2 h-full overflow-clip max-h-full rounded-xl bg-cover" style={{background:`url(${bgImage})`, backgroundPosition:'center'}}>
-      <div className="flex w-full justify-items-stretch items justify-between">
-      <div className="text-lg py-0 active:animate-pulse bg-[#ffffff88] w-max p-2  rounded-xl" 
-      onClick={()=>{
-        console.log(myShortId)
-        navigator.clipboard.writeText(myShortId);
-      }}>
-        {myShortId}
-      <CopyAllRounded />
+      <div className="flex w-min justify-items-center justify-center  items-center mx-auto">
+        <div className="w-32 flex items-center justify-center font-bold">
+          <div className="w-24 h-24 aspect-square flex items-center justify-center text-2xl bg-white p-4 rounded-xl bg-opacity-50">
+          {myShortId}
+          </div>
+        </div>
+        <div className="flex-col w-min items-center justify-center place-ite">
+        <QRShare className="w-24 h-24 left-0 overflow-clip flex-shrink border-none bg-white"/>
+        </div>
+      {/* </div> */}
+      <div className="w-32 h-32 flex items-center justify-center">
+        <div className="w-24 h-24 flex items-center justify-center p-4 bg-white bg-opacity-50 rounded-xl">
+
+        <MuteToggle  />
+        </div>
       </div>
-      <MuteToggle />
+
       </div>
-        <QRShare className="w-32 h-32 left-0 overflow-clip"/>
-      <div className="flex flex-wrap  bg-transparent pt-4 gap-2 max-h-full w-full justify-center">
+      <div className="flex flex-wrap bg-transparent pt-4 gap-1 max-h-full w-full justify-center">
         {players.map((player)=>(
           <PlayerCard key={player.id} player={player} showGold={true} showPoints={true} showItems={true} className={`flex-shrink ${activePlayers.includes(player.id) && 'bg-green-400'} ${gameMode == GAME_MODE.MOVEMENT && (player.movesRemaining > 0 ? 'bg-blue-400': 'bg-green-400')} bg-[#88888888] border-black border-2 `} />
           ))}
