f2a8a019606d5e7c618129100baef334eb966ace
Dan <danbermantech@gmail.com>
2026-04-28 01:59:21 -0400

    Remove round indicator from player map

--- src/components/pixi/PixiClient.tsx
+++ src/components/pixi/PixiClient.tsx
@@ -2,7 +2,6 @@ import { Sprite, } from '@pixi/react';
 import {bg} from '$assets/images';
 import useBoardDimensions from '$hooks/useBoardDimensions';
 import WrappedStage from './WrappedStage';
-import RoundCounter from './RoundCounter';
 import Board from './Board';
 
 export const PixiHost = () =>
@@ -12,7 +11,6 @@ export const PixiHost = () =>
     //@ts-expect-error this is a WrappedStage bug
     <WrappedStage className="w-full mx-auto rounded-xl" width={boardWidth} height={boardHeight} options={{ backgroundColor: 0xffffff, antialias: true }}>
       <Sprite x={0} y={0} width={boardWidth} height={boardHeight} image={bg} scale={{x:boardWidth/1920, y: boardHeight/1080}} />
-      <RoundCounter />
       <Board />
     </WrappedStage>
   );
